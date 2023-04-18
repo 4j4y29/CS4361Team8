d2e45238c0bae4fd447675f1567385b0292237f9
Late-Night-Coding <amer_alzoubi@hotmail.com>
2023-04-17 22:24:45 -0500

    initial map-files

---
+++ Assets/gjm/annimatorControllers/ballisticsLabDoorScript_gjm.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ballisticsLabDoorScript_gjm : MonoBehaviour
+{
+
+
+    public Animator doorAnimation;
+
+    // * Just for debugging
+    public bool isOpeningLocal; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        doorAnimation = this.transform.parent.GetComponent<Animator>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        doorAnimation.SetBool("isOpening", true);
+
+        isOpeningLocal = true;
+    }
+
+
+    void OnTriggerExit(Collider other)
+    {
+        doorAnimation.SetBool("isOpening", false);
+
+        isOpeningLocal = false;
+    }
+
+
+
+
+}
